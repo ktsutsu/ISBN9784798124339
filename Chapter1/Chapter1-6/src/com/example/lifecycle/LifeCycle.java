98aae604d528396f79195ade851e102a253adf55
ktsutsu <tsutsu0422@googlemail.com>
2012-11-05 17:07:23 +0900

    ログ出力用に各ライフサイクルメソッド追加

--- Chapter1/Chapter1-6/src/com/example/lifecycle/LifeCycle.java
+++ Chapter1/Chapter1-6/src/com/example/lifecycle/LifeCycle.java
@@ -1,14 +1,53 @@
 package com.example.lifecycle;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.app.Activity;
 
 public class LifeCycle extends Activity {
+	private static final String TAG = "LifeCycle";
 
-    @Override
+	/** Called when the activity is first created. */
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v(TAG, "onCreate is called.");
         setContentView(R.layout.activity_life_cycle);
     }
 
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.v(TAG, "onStart is called.");
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.v(TAG, "onRestart is called.");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.v(TAG, "onResume is called.");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.v(TAG, "onPause is called.");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.v(TAG, "onStop is called.");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.v(TAG, "onDestroy is called.");
+	}
 }
