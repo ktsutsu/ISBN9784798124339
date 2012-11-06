e5a58ab8f2743244eb13008aa35cc09972d9d677
ktsutsu <tsutsu0422@googlemail.com>
2012-11-06 16:35:04 +0900

    「2 なつかしの数字合わせパズル」用プロジェクト作成

---
+++ Chapter2/src/com/example/numericpuzzle/NumericPuzzle.java
@@ -0,0 +1,13 @@
+package com.example.numericpuzzle;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NumericPuzzle extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numeric_puzzle);
+    }
+}
