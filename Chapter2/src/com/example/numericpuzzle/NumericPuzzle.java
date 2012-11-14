8291e37bcb187c1e2640f6ed47291806305a8d8b
ktsutsu <tsutsu0422@googlemail.com>
2012-11-14 16:08:12 +0900

    画像リソースid配列を作成

--- Chapter2/src/com/example/numericpuzzle/NumericPuzzle.java
+++ Chapter2/src/com/example/numericpuzzle/NumericPuzzle.java
@@ -11,6 +11,13 @@ public class NumericPuzzle extends Activity {
 			R.id.image_button9, R.id.image_button10, R.id.image_button11,
 			R.id.image_button12, R.id.image_button13, R.id.image_button14,
 			R.id.image_button15, R.id.image_button16 };
+	// 画像リソースidリスト
+	static final int numImages[] = { R.drawable.num1, R.drawable.num2,
+			R.drawable.num3, R.drawable.num4, R.drawable.num5, R.drawable.num6,
+			R.drawable.num7, R.drawable.num8, R.drawable.num9,
+			R.drawable.num10, R.drawable.num11, R.drawable.num12,
+			R.drawable.num13, R.drawable.num14, R.drawable.num15,
+			R.drawable.blank };
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
