b4da5d136bb6d329c85f7e30041769606dfd4671
ktsutsu <tsutsu0422@googlemail.com>
2012-11-14 16:09:05 +0900

    画像ボタンのOnClickListenerイベントをインプリメント

--- Chapter2/src/com/example/numericpuzzle/NumericPuzzle.java
+++ Chapter2/src/com/example/numericpuzzle/NumericPuzzle.java
@@ -1,6 +1,7 @@
 package com.example.numericpuzzle;
 
 import android.os.Bundle;
+import android.view.View;
 import android.app.Activity;
 
 public class NumericPuzzle extends Activity {
@@ -23,5 +24,13 @@ public class NumericPuzzle extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_numeric_puzzle);
+	}
+
+	class OrderController implements View.OnClickListener {
+
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+
+		}
 	}
 }
