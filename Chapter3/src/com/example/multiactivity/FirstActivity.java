3c058678c610c3c299ae66c419e9b21240f85e47
ktsutsu <tsutsu0422@googlemail.com>
2012-11-30 20:07:44 +0900

    ボタンクリック時の挙動追加

--- Chapter3/src/com/example/multiactivity/FirstActivity.java
+++ Chapter3/src/com/example/multiactivity/FirstActivity.java
@@ -1,7 +1,11 @@
 package com.example.multiactivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.app.Activity;
+import android.content.Intent;
 
 public class FirstActivity extends Activity {
 
@@ -9,6 +13,15 @@ public class FirstActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_first);
+		Button btnNext = (Button) this.findViewById(R.id.Button01);
+		btnNext.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(FirstActivity.this,
+						SecondActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 }
