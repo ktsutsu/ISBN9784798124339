548d214966b3fec35154e1312aa232ce1924af71
ktsutsu <tsutsu0422@googlemail.com>
2012-12-07 19:40:32 +0900

    暗黙的なインテント追加

--- Chapter3/src/com/example/multiactivity/FirstActivity.java
+++ Chapter3/src/com/example/multiactivity/FirstActivity.java
@@ -1,5 +1,6 @@
 package com.example.multiactivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,6 +20,15 @@ public class FirstActivity extends Activity {
 			public void onClick(View v) {
 				Intent intent = new Intent(FirstActivity.this,
 						SecondActivity.class);
+				startActivity(intent);
+			}
+		});
+		Button btnGoogle = (Button) this.findViewById(R.id.Button02);
+		btnGoogle.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Uri uri = Uri.parse("http://www.google.co.jp");
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 				startActivity(intent);
 			}
 		});
