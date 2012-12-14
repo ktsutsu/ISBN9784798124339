cd98a41e539b83fdacdfdd8765fe2d094ecddd1d
ktsutsu <tsutsu0422@googlemail.com>
2012-12-14 16:36:55 +0900

    レイアウトを作成

--- LimitedCall/src/com/example/limitedcall/LimitedCall.java
+++ LimitedCall/src/com/example/limitedcall/LimitedCall.java
@@ -1,6 +1,7 @@
 package com.example.limitedcall;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.app.Activity;
 
 public class LimitedCall extends Activity {
@@ -8,5 +9,9 @@ public class LimitedCall extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// レイアウトの作成
+		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
+		setContentView(layout);
 	}
 }
