76c5fd1cd17ba24b8f7c29c5741198f23aee5b96
ktsutsu <tsutsu0422@googlemail.com>
2012-12-14 16:23:53 +0900

    「○○ちゃんに電話」用プロジェクト作成

---
+++ LimitedCall/src/com/example/limitedcall/LimitedCall.java
@@ -0,0 +1,12 @@
+package com.example.limitedcall;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class LimitedCall extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
