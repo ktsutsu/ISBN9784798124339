f47953b36045584a64d55e8b57cc7e2d71c57983
ktsutsu <tsutsu0422@googlemail.com>
2012-12-14 18:18:36 +0900

    「のみにいくめしいらない」用プロジェクト作成

---
+++ LimitedMail/src/com/example/limitedmail/LimitedMail.java
@@ -0,0 +1,12 @@
+package com.example.limitedmail;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class LimitedMail extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
