297b66a032f03c35cdb69bb8a0a734c643ddc9cc
ktsutsu <tsutsu0422@googlemail.com>
2012-12-14 18:20:29 +0900

    エディットテキスト（メールアドレス）作成

--- LimitedMail/src/com/example/limitedmail/LimitedMail.java
+++ LimitedMail/src/com/example/limitedmail/LimitedMail.java
@@ -2,10 +2,12 @@ package com.example.limitedmail;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.app.Activity;
 
 public class LimitedMail extends Activity implements View.OnClickListener {
+	private EditText edit01;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,13 @@ public class LimitedMail extends Activity implements View.OnClickListener {
 		LinearLayout layout = new LinearLayout(this);
 		layout.setOrientation(LinearLayout.VERTICAL);
 		setContentView(layout);
+		// エディットテキスト（メールアドレス）の作成
+		edit01 = new EditText(this);
+		edit01.setText("aaaaa@bbbbbbbbb.com");
+		edit01.setLayoutParams(new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.FILL_PARENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT));
+		layout.addView(edit01);
 	}
 
 	@Override
