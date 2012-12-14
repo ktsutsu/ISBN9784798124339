bf0ee60714f4ab3a2220dd64d8c382999e99c2d1
ktsutsu <tsutsu0422@googlemail.com>
2012-12-14 18:19:26 +0900

    OnClickListenerをインプリメント

--- LimitedMail/src/com/example/limitedmail/LimitedMail.java
+++ LimitedMail/src/com/example/limitedmail/LimitedMail.java
@@ -1,12 +1,18 @@
 package com.example.limitedmail;
 
 import android.os.Bundle;
+import android.view.View;
 import android.app.Activity;
 
-public class LimitedMail extends Activity {
+public class LimitedMail extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
 	}
 }
