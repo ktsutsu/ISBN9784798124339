83aa5e57bcf2a6f512a838fa464601ff2c03caee
ktsutsu <tsutsu0422@googlemail.com>
2012-12-17 19:19:14 +0900

    OnClickListenerをインプリメント

--- ServerCheck/src/com/example/servercheck/ServerCheck.java
+++ ServerCheck/src/com/example/servercheck/ServerCheck.java
@@ -1,12 +1,18 @@
 package com.example.servercheck;
 
 import android.os.Bundle;
+import android.view.View;
 import android.app.Activity;
 
-public class ServerCheck extends Activity {
+public class ServerCheck extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
 	}
 }
