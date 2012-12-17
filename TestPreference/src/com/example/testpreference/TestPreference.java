13e13bc739a7b1a18d908aabacf71b0c34ba45f3
ktsutsu <tsutsu0422@googlemail.com>
2012-12-17 18:46:21 +0900

    OnClickListenerをインプリメント

--- TestPreference/src/com/example/testpreference/TestPreference.java
+++ TestPreference/src/com/example/testpreference/TestPreference.java
@@ -1,12 +1,17 @@
 package com.example.testpreference;
 
 import android.os.Bundle;
+import android.view.View;
 import android.app.Activity;
 
-public class TestPreference extends Activity {
-
+public class TestPreference extends Activity implements View.OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
 	}
 }
