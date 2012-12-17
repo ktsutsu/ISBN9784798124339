8439f21285ed3204ef534dc441275da2627e19f8
ktsutsu <tsutsu0422@googlemail.com>
2012-12-17 18:43:32 +0900

    「プリファレンステスト」用プロジェクト作成

---
+++ TestPreference/src/com/example/testpreference/TestPreference.java
@@ -0,0 +1,12 @@
+package com.example.testpreference;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TestPreference extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
