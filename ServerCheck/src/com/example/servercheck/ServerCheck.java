a764bddd44ecddc4e47471bbde161d09c7e60eaf
ktsutsu <tsutsu0422@googlemail.com>
2012-12-17 19:18:00 +0900

    「休日にサーバー状態チェックツール」用プロジェクト作成

---
+++ ServerCheck/src/com/example/servercheck/ServerCheck.java
@@ -0,0 +1,12 @@
+package com.example.servercheck;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ServerCheck extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
