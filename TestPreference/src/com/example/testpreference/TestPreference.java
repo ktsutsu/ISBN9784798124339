aac91d4a3bb4d70b0b269d061bb40e6da14b129a
ktsutsu <tsutsu0422@googlemail.com>
2012-12-17 18:50:35 +0900

    入力ボックスを作成

--- TestPreference/src/com/example/testpreference/TestPreference.java
+++ TestPreference/src/com/example/testpreference/TestPreference.java
@@ -2,10 +2,13 @@ package com.example.testpreference;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.app.Activity;
 
 public class TestPreference extends Activity implements View.OnClickListener {
+	EditText edit01;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -13,6 +16,13 @@ public class TestPreference extends Activity implements View.OnClickListener {
 		LinearLayout layout = new LinearLayout(this);
 		layout.setOrientation(LinearLayout.VERTICAL);
 		setContentView(layout);
+		// 入力ボックスの作成
+		edit01 = new EditText(this);
+		edit01.setText("");
+		edit01.setLayoutParams(new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.FILL_PARENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT));
+		layout.addView(edit01);
 	}
 
 	@Override
