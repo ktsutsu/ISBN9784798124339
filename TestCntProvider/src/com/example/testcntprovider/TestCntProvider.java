774275b9d0462d44547fbcb9804742fcf41c8e9b
ktsutsu <tsutsu0422@googlemail.com>
2012-12-18 19:00:12 +0900

    取得するボタンを作成

--- TestCntProvider/src/com/example/testcntprovider/TestCntProvider.java
+++ TestCntProvider/src/com/example/testcntprovider/TestCntProvider.java
@@ -1,10 +1,12 @@
 package com.example.testcntprovider;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.app.Activity;
 
 public class TestCntProvider extends Activity {
+	private Button btnQuery;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +15,13 @@ public class TestCntProvider extends Activity {
 		LinearLayout layout = new LinearLayout(this);
 		layout.setOrientation(LinearLayout.VERTICAL);
 		setContentView(layout);
+
+		// 取得するボタンを作成
+		btnQuery = new Button(this);
+		btnQuery.setText("取得する");
+		btnQuery.setLayoutParams(new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.FILL_PARENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT));
+		layout.addView(btnQuery);
 	}
 }
