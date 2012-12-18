8ccd383c7a2f51b414b6e0e77d194d200e33bc65
ktsutsu <tsutsu0422@googlemail.com>
2012-12-18 18:56:40 +0900

    レイアウト作成

--- TestCntProvider/src/com/example/testcntprovider/TestCntProvider.java
+++ TestCntProvider/src/com/example/testcntprovider/TestCntProvider.java
@@ -1,6 +1,7 @@
 package com.example.testcntprovider;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.app.Activity;
 
 public class TestCntProvider extends Activity {
@@ -8,5 +9,9 @@ public class TestCntProvider extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// レイアウトの作成
+		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
+		setContentView(layout);
 	}
 }
