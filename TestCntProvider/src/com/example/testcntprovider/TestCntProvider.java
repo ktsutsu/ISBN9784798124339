a4b86d60065587d96bf5147e1d22ee1683489733
ktsutsu <tsutsu0422@googlemail.com>
2012-12-18 19:04:29 +0900

    取得結果表示エリアを作成

--- TestCntProvider/src/com/example/testcntprovider/TestCntProvider.java
+++ TestCntProvider/src/com/example/testcntprovider/TestCntProvider.java
@@ -3,10 +3,12 @@ package com.example.testcntprovider;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.app.Activity;
 
 public class TestCntProvider extends Activity {
 	private Button btnQuery;
+	private TextView txtView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,14 @@ public class TestCntProvider extends Activity {
 				LinearLayout.LayoutParams.FILL_PARENT,
 				LinearLayout.LayoutParams.WRAP_CONTENT));
 		layout.addView(btnQuery);
+
+		// 取得結果表示エリアを作成
+		txtView = new TextView(this);
+		txtView.setText("取得結果をここに表示します");
+		txtView.setLayoutParams(new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.FILL_PARENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT));
+		layout.addView(txtView);
+
 	}
 }
