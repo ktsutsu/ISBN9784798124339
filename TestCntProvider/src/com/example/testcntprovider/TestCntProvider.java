b6be65c17cd8e6ebdd638188b71b894f34c8a4ca
ktsutsu <tsutsu0422@googlemail.com>
2012-12-18 18:55:10 +0900

    「コンテントプロバイダテスト」用プロジェクト作成

---
+++ TestCntProvider/src/com/example/testcntprovider/TestCntProvider.java
@@ -0,0 +1,12 @@
+package com.example.testcntprovider;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TestCntProvider extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
