ec3789b4f0fc229d994c4d9fc9696239e103971c
ktsutsu <tsutsu0422@googlemail.com>
2012-12-18 20:09:31 +0900

    取得するボタン押下時の処理作成

--- TestCntProvider/src/com/example/testcntprovider/TestCntProvider.java
+++ TestCntProvider/src/com/example/testcntprovider/TestCntProvider.java
@@ -1,11 +1,13 @@
 package com.example.testcntprovider;
 
 import android.os.Bundle;
+import android.provider.ContactsContract.Contacts;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.app.Activity;
+import android.database.Cursor;
 
 public class TestCntProvider extends Activity implements View.OnClickListener {
 	private Button btnQuery;
@@ -22,6 +24,7 @@ public class TestCntProvider extends Activity implements View.OnClickListener {
 		// 取得するボタンを作成
 		btnQuery = new Button(this);
 		btnQuery.setText("取得する");
+		btnQuery.setOnClickListener(this);
 		btnQuery.setLayoutParams(new LinearLayout.LayoutParams(
 				LinearLayout.LayoutParams.FILL_PARENT,
 				LinearLayout.LayoutParams.WRAP_CONTENT));
@@ -39,6 +42,48 @@ public class TestCntProvider extends Activity implements View.OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
+		getContactsData();
+	}
+
+	// 連絡先を取得して、結果エリアに表示する
+	private void getContactsData() {
+		String[] proj = new String[] { Contacts._ID, Contacts.DISPLAY_NAME,
+				Contacts.HAS_PHONE_NUMBER };
+		// 連絡先を取得
+		Cursor cur = managedQuery(Contacts.CONTENT_URI, proj, null, null,
+				Contacts.DISPLAY_NAME + " ASC");
+
+		// 結果が空なら戻る
+		if (!cur.moveToFirst()) {
+			txtView.setText("");
+			return;
+		}
+
+		StringBuffer buff = new StringBuffer();
+		String _id;
+		String name;
+		String hasPhoneNumber;
+		// カラム名からインデックスを取得
+		int _idColumn = cur
+				.getColumnIndex(android.provider.ContactsContract.Contacts._ID);
+		int nameColumn = cur
+				.getColumnIndex(android.provider.ContactsContract.Contacts.DISPLAY_NAME);
+		int phoneColumn = cur
+				.getColumnIndex(android.provider.ContactsContract.Contacts.HAS_PHONE_NUMBER);
+		do {
+			// フィールド値を取得する
+			_id = cur.getString(_idColumn);
+			name = cur.getString(nameColumn);
+			hasPhoneNumber = cur.getString(phoneColumn);
+
+			buff.append(_id);
+			buff.append(" : ");
+			buff.append(name);
+			buff.append(" : ");
+			buff.append(hasPhoneNumber);
+			buff.append("\n");
+		} while (cur.moveToNext());
+		cur.close();
+		txtView.setText(buff.toString());
 	}
 }
