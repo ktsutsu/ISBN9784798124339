b3039c994cddae3da3c81600bc8b4051609547c5
ktsutsu <tsutsu0422@googlemail.com>
2013-01-06 09:52:25 +0900

    TextView作成

--- PhotoAlbum/src/com/example/photoalbum/PhotoAlbum.java
+++ PhotoAlbum/src/com/example/photoalbum/PhotoAlbum.java
@@ -5,8 +5,11 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class PhotoAlbum extends Activity {
+    private TextView txtView;
+
     /**
      * Called when the activity is first created.
      */
@@ -18,5 +21,14 @@ public class PhotoAlbum extends Activity {
         layout.setBackgroundColor(Color.rgb(0, 0, 255));
         layout.setGravity(Gravity.CENTER_HORIZONTAL);
         setContentView(layout);
+
+        // TextViewの生成
+        txtView = new TextView(this);
+        txtView.setText("日付を表示");
+        txtView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        layout.addView(txtView);
     }
 }
