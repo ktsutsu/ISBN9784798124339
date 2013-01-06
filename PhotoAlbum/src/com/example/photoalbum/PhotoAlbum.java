9a6f4fd92293e2b90720b968c0f3d69ae80d73de
ktsutsu <tsutsu0422@googlemail.com>
2013-01-06 09:23:24 +0900

    「思い出のアルバム」用プロジェクト作成

---
+++ PhotoAlbum/src/com/example/photoalbum/PhotoAlbum.java
@@ -0,0 +1,14 @@
+package com.example.photoalbum;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhotoAlbum extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
