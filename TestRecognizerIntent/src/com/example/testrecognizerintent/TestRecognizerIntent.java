76d9befbb5f204735ed94106f89847d853629d27
ktsutsu <tsutsu0422@googlemail.com>
2013-01-08 07:15:53 +0900

    開始ボタンクリック時の処理追加

--- TestRecognizerIntent/src/com/example/testrecognizerintent/TestRecognizerIntent.java
+++ TestRecognizerIntent/src/com/example/testrecognizerintent/TestRecognizerIntent.java
@@ -1,12 +1,17 @@
 package com.example.testrecognizerintent;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class TestRecognizerIntent extends Activity implements View.OnClickListener {
+    private static final int REQUEST_CODE = 0;
     private Button btnStart;
 
     /**
@@ -22,6 +27,7 @@ public class TestRecognizerIntent extends Activity implements View.OnClickListen
         // 開始ボタンを生成
         btnStart = new Button(this);
         btnStart.setText("開始");
+        btnStart.setOnClickListener(this);
         btnStart.setLayoutParams(new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.FILL_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
@@ -31,6 +37,19 @@ public class TestRecognizerIntent extends Activity implements View.OnClickListen
 
     @Override
     public void onClick(View view) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        try {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            // 言語モデル設定
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            // マイクアイコン下のテキスト設定
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "音声認識");
+            // 結果を1つだけ取得
+            intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+            startActivityForResult(intent, REQUEST_CODE);
+        } catch (ActivityNotFoundException e) {
+            // 音声認識機能がない場合エラーをトースト表示
+            Toast.makeText(TestRecognizerIntent.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
