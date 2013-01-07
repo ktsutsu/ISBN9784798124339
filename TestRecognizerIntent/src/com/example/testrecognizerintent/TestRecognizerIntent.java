96664ddd7e062220526d19be5ad47a1206b84775
ktsutsu <tsutsu0422@googlemail.com>
2013-01-08 07:14:15 +0900

    開始ボタン生成

--- TestRecognizerIntent/src/com/example/testrecognizerintent/TestRecognizerIntent.java
+++ TestRecognizerIntent/src/com/example/testrecognizerintent/TestRecognizerIntent.java
@@ -2,9 +2,12 @@ package com.example.testrecognizerintent;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class TestRecognizerIntent extends Activity {
+    private Button btnStart;
+
     /**
      * Called when the activity is first created.
      */
@@ -14,5 +17,14 @@ public class TestRecognizerIntent extends Activity {
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
         setContentView(layout);
+
+        // 開始ボタンを生成
+        btnStart = new Button(this);
+        btnStart.setText("開始");
+        btnStart.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        layout.addView(btnStart);
     }
 }
