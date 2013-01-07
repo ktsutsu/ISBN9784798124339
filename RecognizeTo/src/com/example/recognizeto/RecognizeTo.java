d129c65948bab75479c2f87cb92af00fc0e78ca8
ktsutsu <tsutsu0422@googlemail.com>
2013-01-08 07:40:03 +0900

    「口述筆記くん」用プロジェクト作成

---
+++ RecognizeTo/src/com/example/recognizeto/RecognizeTo.java
@@ -0,0 +1,14 @@
+package com.example.recognizeto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecognizeTo extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
