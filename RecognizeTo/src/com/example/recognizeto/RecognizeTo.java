ac057ee672bcc3bdea55666c5ff7d1db58f0636e
ktsutsu <tsutsu0422@googlemail.com>
2013-01-08 07:45:09 +0900

    開始ボタン生成

--- RecognizeTo/src/com/example/recognizeto/RecognizeTo.java
+++ RecognizeTo/src/com/example/recognizeto/RecognizeTo.java
@@ -2,9 +2,12 @@ package com.example.recognizeto;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RecognizeTo extends Activity {
+    private Button btnStart;
+
     /**
      * Called when the activity is first created.
      */
@@ -14,5 +17,13 @@ public class RecognizeTo extends Activity {
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
         setContentView(layout);
+
+        // 開始ボタンを生成
+        btnStart = new Button(this);
+        btnStart.setText("開始");
+        btnStart.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(btnStart);
     }
 }
