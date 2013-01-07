ddf228468bf3c398a0dde42790225abbaffbab0c
ktsutsu <tsutsu0422@googlemail.com>
2013-01-08 05:55:15 +0900

    「音声認識プログラミングの基本」用プロジェクト作成

---
+++ TestRecognizerIntent/src/com/example/testrecognizerintent/TestRecognizerIntent.java
@@ -0,0 +1,14 @@
+package com.example.testrecognizerintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestRecognizerIntent extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
